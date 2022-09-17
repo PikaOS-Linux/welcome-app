eb059c3a5957d99c3fe9ce701b44e29ae426103a
Ward Nakchbandi <hotrod.master@hotmail.com>
2022-09-17 18:05:11 +0300

    commit

--- __main__.py
+++ __main__.py
@@ -1,12 +1,10 @@
 import gi
-import pandas as pd
-from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg
-import  matplotlib.pyplot as plt
-
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 class Application:
+    
+    ### MAIN WINDOW ###
     def __init__(self):
         self.column_names = False
         self.drop_nan = False
@@ -17,7 +15,32 @@ class Application:
         self.builder.connect_signals(self)
         
         self.window = self.builder.get_object("main_Window")
-        self.window.show_all()
+        self.window.show()
+    ### LOOK WINDOW ###
+    def enter_look(self, widget):
+        look_window =  self.builder.get_object("look_Window")
+        look_window.show()
+    ### DRIVER WINDOW ###
+    def enter_driver(self, widget):
+        look_window =  self.builder.get_object("look_Window")
+        look_window.show()
+    ### APP WINDOW ###
+    def enter_apps(self, widget):
+        look_window =  self.builder.get_object("look_Window")
+        look_window.show()
+    ### DOC WINDOW ###
+    def enter_doc(self, widget):
+        look_window =  self.builder.get_object("look_Window")
+        look_window.show()
+    ### PROB WINDOW ###
+    def enter_prob(self, widget):
+        look_window =  self.builder.get_object("look_Window")
+        look_window.show()
+    ### WEBSITE WINDOW ###
+    def enter_website(self, widget):
+        look_window =  self.builder.get_object("look_Window")
+        look_window.show()
+
 if __name__ == "__main__":
     Application()
     Gtk.main()
