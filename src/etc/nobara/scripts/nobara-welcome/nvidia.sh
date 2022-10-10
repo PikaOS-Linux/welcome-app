a3835319cf34249e5a116cf4bbd8bfe0360faf22
GloriousEggroll <gloriouseggroll@gmail.com>
2022-10-10 01:40:04 -0600

    ... I forgot we can't put an notice in the nvidia checker or it spams the user

--- src/etc/nobara/scripts/nobara-welcome/nvidia.sh
+++ src/etc/nobara/scripts/nobara-welcome/nvidia.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
-/usr/bin/hwcheck
+nvgpu=$(lspci | grep -iEA3 '^[[:digit:]]{2}:[[:digit:]]{2}.*VGA|3D' | grep -i nvidia | cut -d ":" -f 3)
 
+/usr/bin/hwcheck
+if [ $? -eq 0 ]; then
+	if [[ -z $nvgpu ]]; then
+    		zenity --info\
+           	--title="No Nvidia GPU detected" \
+           	--width=600 \
+           	--text="`printf "No Nvidia GPU has been detected on the system.\n\n"`"
+           	exit 0
+        fi
+fi
