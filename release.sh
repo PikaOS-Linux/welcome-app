3e319b3501e040ef5a107b0fe11819c62e8bdb18
ferrreo <harderthanfire@gmail.com>
2023-09-06 22:15:28 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,14 +1,8 @@
-# Sign the packages
-dpkg-sig --sign builder ./output/pika-welcome*.deb
+# send debs to server
+rsync -azP --include './' --include '*.deb' --exclude '*' ./output/ ferreo@direct.pika-os.com:/srv/www/incoming/
 
-# Pull down existing ppa repo db files etc
-rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
+# add debs to repo
+ssh ferreo@direct.pika-os.com 'aptly repo add -force-replace -remove-files pika-main /srv/www/incoming/'
 
-# Remove our existing package from the repo
-reprepro -V -C main --basedir ./output/repo/ removefilter lunar 'Package (% pika-welcome*)'
-
-# Add the new package to the repo
-reprepro -V -C main --basedir ./output/repo/ includedeb lunar ./output/pika-welcome*.deb
-
-# Push the updated ppa repo to the server
-rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
+# publish the repo
+ssh ferreo@direct.pika-os.com 'aptly publish update -batch -skip-contents -force-overwrite lunar filesystem:pikarepo:'
